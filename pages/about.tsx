65e128e96a02a7b142d956ee21dfc11c6d8480da
Asney <asney28825@gmail.com>
2022-07-25 17:50:44 -0400

    feat: Clean unnecessary default data and separate in some components

---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+import ComingSoonCard from '../components/ComingSoonCard';
+
+const About: NextPage = () => {
+  return <ComingSoonCard />;
+};
+
+export default About;
---
+++ pages/services.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+import ComingSoonCard from '../components/ComingSoonCard';
+
+const Services: NextPage = () => {
+  return <ComingSoonCard />;
+};
+
+export default Services;
